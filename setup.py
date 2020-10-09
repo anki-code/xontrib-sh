9bf3937ad4495e8ba0f6477eea8ecf00c02c1816
a <a>
2020-10-10 01:27:06 +0300

    0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except (IOError, OSError):
 
 setup(
     name='xontrib-sh',
-    version='0.0.4',
+    version='0.0.5',
     license='BSD',
     author='anki',
     author_email='author@example.com',
